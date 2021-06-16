b89784c0e91053ccb7e1bfb3081087f51af6e315
aki <aki.akaguma@hotmail.com>
2021-06-17 00:20:21 +0900

    version 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@ TBD
 ===
 Unreleased changes. Release notes have not yet been written.
 
+0.1.1 (2021-06-16)
+=====
+
+* armv7-unknown-linux-musleabihf: can not compile: not found `__clear_cache()`
+* suppress this error by void function
+
 0.1.0 (2021-06-16)
 =====
 
--- build.rs
+++ build.rs
@@ -7,10 +7,22 @@ fn main() {
 fn compile_c_src() {
     println!("cargo:rerun-if-changed=src/c/clf.c");
     //
-    cc::Build::new()
-        .file("src/c/clf.c")
-        .flag_if_supported("-Wno-unused-function")
-        //.shared_flag(true)
-        .static_flag(true)
-        .compile("libclf.a");
+    let env_target = std::env::var("TARGET").unwrap();
+    if  env_target.as_str() != "armv7-unknown-linux-musleabihf" {
+        cc::Build::new()
+            .file("src/c/clf.c")
+            .flag_if_supported("-Wno-unused-function")
+            //.shared_flag(true)
+            .static_flag(true)
+            .compile("libclf.a");
+    } else {
+        std::env::set_var("CC_armv7-unknown-linux-musleabihf", "arm-linux-gnueabihf-gcc");
+        cc::Build::new()
+            .file("src/c/clf-void.c")
+            .flag_if_supported("-Wno-unused-function")
+            .flag_if_supported("-Wno-unused-parameter")
+            //.shared_flag(true)
+            .static_flag(true)
+            .compile("libclf.a");
+    }
 }
